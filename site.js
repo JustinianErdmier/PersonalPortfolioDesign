8b80d2f58baee38dc5abd2e983d87bca9c10de6c
JustinErdmier <justin.erdmier@outlook.com>
2022-07-09 01:53:04 -0500

    Finish skills section.

--- site.js
+++ site.js
@@ -6,3 +6,21 @@ $(window).scroll(() => {
     if (scroll >= 100) header.addClass(scrolledClass);
     else header.removeClass(scrolledClass);
 });
+
+/**
+ * Opens a specified modal by element id.
+ *
+ * @param modalId
+ */
+function openModal(modalId) {
+    $('#' + modalId).show();
+}
+
+/**
+ * Closes a specified modal by element id.
+ *
+ * @param modalId
+ */
+function closeModal(modalId) {
+    $('#' + modalId).hide();
+}
